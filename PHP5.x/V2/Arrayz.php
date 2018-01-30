4aeae3795c66514673af889bf36f1282ce618091
Giri Annamalai M <curiegiri48@gmail.com>
2018-01-30 17:44:07 +0530

    update func updated

--- PHP5.x/V2/Arrayz.php
+++ PHP5.x/V2/Arrayz.php
@@ -1096,7 +1096,7 @@ class Arrayz
 		array_walk($this->source, function(&$v, &$k) use ($update_data) {
 			$v = array_replace($v, $update_data);
 		});
-		$this->source = array_merge($this->source,$this->orig_source);
+		$this->source = array_replace($this->orig_source,$this->source);
 		return $this;
 	}
 	public function __call($name, $arguments)
